dbbe1d907bc757a89bc7b3b7f94b251965a873e7
Leonardo Santos <leonardosantosneo037@gmail.com>
2021-03-04 23:04:43 -0300

    Angular diretivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'diretivas';
+  pessoas = [];
+
+  onAdicionarPessoa(pessoa){
+    this.pessoas = [pessoa, ...this.pessoas];
+  }
+
+  //textoBotao = "Esconder";
+  //esconder = false;
+
+  //alterarExibicao(){
+  //  this.esconder = !this.esconder;
+  //  this.textoBotao = this.esconder ? "Exibir" : "Esconder";
+  //}
 }
